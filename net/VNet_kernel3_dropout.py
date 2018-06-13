3f685f768ff66c394031f76e167cfda5414bcfef
Zcy <145958061@qq.com>
2018-06-13 23:16:36 +0800

    complet net

---
+++ net/VNet_kernel3_dropout.py
@@ -0,0 +1,220 @@
+"""
+
+与kernel3版本的区别在于在参数量最大的六层增加了dropout防止过拟合，效果提升很小
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class VNet(nn.Module):
+    """
+
+    网络一共有22层，如果包含上采样和下采样，则一共有30层
+    共16302656个可训练的参数, 一千六百万左右
+    """
+    def __init__(self, training):
+        super().__init__()
+
+        self.training = training
+
+        self.encoder_stage1 = nn.Sequential(
+            nn.Conv3d(1, 16, 3, 1, padding=1),
+            nn.PReLU(16),
+        )
+
+        self.encoder_stage2 = nn.Sequential(
+            nn.Conv3d(32, 32, 3, 1, padding=1),
+            nn.PReLU(32),
+
+            nn.Conv3d(32, 32, 3, 1, padding=1),
+            nn.PReLU(32),
+        )
+
+        self.encoder_stage3 = nn.Sequential(
+            nn.Conv3d(64, 64, 3, 1, padding=1),
+            nn.PReLU(64),
+
+            nn.Conv3d(64, 64, 3, 1, padding=1),
+            nn.PReLU(64),
+
+            nn.Conv3d(64, 64, 3, 1, padding=1),
+            nn.PReLU(64),
+        )
+
+        self.encoder_stage4 = nn.Sequential(
+            nn.Conv3d(128, 128, 3, 1, padding=1),
+            nn.PReLU(128),
+
+            nn.Conv3d(128, 128, 3, 1, padding=1),
+            nn.PReLU(128),
+
+            nn.Conv3d(128, 128, 3, 1, padding=1),
+            nn.PReLU(128),
+        )
+
+        self.encoder_stage5 = nn.Sequential(
+            nn.Conv3d(256, 256, 3, 1, padding=1),
+            nn.PReLU(256),
+
+            nn.Conv3d(256, 256, 3, 1, padding=1),
+            nn.PReLU(256),
+
+            nn.Conv3d(256, 256, 3, 1, padding=1),
+            nn.PReLU(256),
+        )
+
+        self.decoder_stage1 = nn.Sequential(
+            nn.Conv3d(256 + 128, 256, 3, 1, padding=1),
+            nn.PReLU(256),
+
+            nn.Conv3d(256, 256, 3, 1, padding=1),
+            nn.PReLU(256),
+
+            nn.Conv3d(256, 256, 3, 1, padding=1),
+            nn.PReLU(256),
+        )
+
+        self.decoder_stage2 = nn.Sequential(
+            nn.Conv3d(128 + 64, 128, 3, 1, padding=1),
+            nn.PReLU(128),
+
+            nn.Conv3d(128, 128, 3, 1, padding=1),
+            nn.PReLU(128),
+
+            nn.Conv3d(128, 128, 3, 1, padding=1),
+            nn.PReLU(128),
+        )
+
+        self.decoder_stage3 = nn.Sequential(
+            nn.Conv3d(64 + 32, 64, 3, 1, padding=1),
+            nn.PReLU(64),
+
+            nn.Conv3d(64, 64, 3, 1, padding=1),
+            nn.PReLU(64),
+        )
+
+        self.decoder_stage4 = nn.Sequential(
+            nn.Conv3d(32 + 16, 32, 3, 1, padding=1),
+            nn.PReLU(32),
+        )
+
+        self.down_conv1 = nn.Sequential(
+            nn.Conv3d(16, 32, 2, 2),
+            nn.PReLU(32)
+        )
+
+        self.down_conv2 = nn.Sequential(
+            nn.Conv3d(32, 64, 2, 2),
+            nn.PReLU(64)
+        )
+
+        self.down_conv3 = nn.Sequential(
+            nn.Conv3d(64, 128, 2, 2),
+            nn.PReLU(128)
+        )
+
+        self.down_conv4 = nn.Sequential(
+            nn.Conv3d(128, 256, 2, 2),
+            nn.PReLU(256)
+        )
+
+        self.up_conv1 = nn.Sequential(
+            nn.ConvTranspose3d(256, 256, 2, 2),
+            nn.PReLU(256)
+        )
+
+        self.up_conv2 = nn.Sequential(
+            nn.ConvTranspose3d(256, 128, 2, 2),
+            nn.PReLU(128)
+        )
+
+        self.up_conv3 = nn.Sequential(
+            nn.ConvTranspose3d(128, 64, 2, 2),
+            nn.PReLU(64)
+        )
+
+        self.up_conv4 = nn.Sequential(
+            nn.ConvTranspose3d(64, 32, 2, 2),
+            nn.PReLU(32)
+        )
+
+        self.map = nn.Sequential(
+            nn.Conv3d(32, 1, 1, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, inputs):
+
+        long_range1 = self.encoder_stage1(inputs) + inputs
+
+        short_range1 = self.down_conv1(long_range1)
+
+        long_range2 = self.encoder_stage2(short_range1) + short_range1
+        long_range2 = F.dropout(long_range2, 0.3, self.training)
+
+        short_range2 = self.down_conv2(long_range2)
+
+        long_range3 = self.encoder_stage3(short_range2) + short_range2
+        long_range3 = F.dropout(long_range3, 0.3, self.training)
+
+        short_range3 = self.down_conv3(long_range3)
+
+        long_range4 = self.encoder_stage4(short_range3) + short_range3
+        long_range4 = F.dropout(long_range4, 0.3, self.training)
+
+        short_range4 = self.down_conv4(long_range4)
+
+        outputs = self.encoder_stage5(short_range4) + short_range4
+        outputs = F.dropout(outputs, 0.3, self.training)
+
+        short_range5 = self.up_conv1(outputs)
+
+        outputs = self.decoder_stage1(torch.cat([short_range5, long_range4], dim=1)) + short_range5
+        outputs = F.dropout(outputs, 0.3, self.training)
+
+        short_range6 = self.up_conv2(outputs)
+
+        outputs = self.decoder_stage2(torch.cat([short_range6, long_range3], dim=1)) + short_range6
+        outputs = F.dropout(outputs, 0.3, self.training)
+
+        short_range7 = self.up_conv3(outputs)
+
+        outputs = self.decoder_stage3(torch.cat([short_range7, long_range2], dim=1)) + short_range7
+        outputs = F.dropout(outputs, 0.3, self.training)
+
+        short_range8 = self.up_conv4(outputs)
+
+        outputs = self.decoder_stage4(torch.cat([short_range8, long_range1], dim=1)) + short_range8
+
+        outputs = self.map(outputs)
+
+        return outputs
+
+
+def init(module):
+    if isinstance(module, nn.Conv3d) or isinstance(module, nn.ConvTranspose3d):
+        nn.init.kaiming_normal(module.weight.data, 0.25)
+        nn.init.constant(module.bias.data, 0)
+
+
+net = VNet(training=True)
+net.apply(init)
+
+# # 输出数据维度检查
+# data = torch.randn((1, 1, 32, 160, 160)).cuda()
+# res = net(data)
+# print(res.size())
+
+# # 计算网络参数
+# num_parameter = .0
+# for item in net.modules():
+#
+#     if isinstance(item, nn.Conv3d) or isinstance(item, nn.ConvTranspose3d):
+#         num_parameter += (item.in_channels * item.out_channels * (item.kernel_size[0] ** 3))
+#
+#     if isinstance(item, nn.PReLU):
+#         num_parameter += item.num_parameters
+#
+# print(num_parameter)
